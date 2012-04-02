84cec6ba1cfc8fdc8211476b8ca97285012a3e2f
ant <iljinaa@gmail.com>
2012-04-02 13:03:21 +0300

    fix

--- ru.ocltd.linq/FilterExpressionTreeBuilder.cs
+++ ru.ocltd.linq/FilterExpressionTreeBuilder.cs
@@ -11,31 +11,49 @@ namespace ru.ocltd.linq
     {
         public static Expression<Func<T, bool>> Build<T>(params Object[] values) where T : class
         {
-            MemberInfo[] members = typeof (T).GetMembers().Where(s=>s.MemberType == MemberTypes.Property).ToArray();
-
             Expression result = null;
 
             foreach (var value in values)
             {
-                foreach (var member in members.Where(s => ((PropertyInfo)s).PropertyType==value.GetType()))
+                foreach (var member in typeof (T).GetProperties())
                 {
-                    ParameterExpression param = Expression.Parameter(typeof(T),"i");
-                    ConstantExpression constant = Expression.Constant(value, value.GetType());
-                    Expression expression = (value is string)
-                        ? (Expression)Expression.Call(Expression.MakeMemberAccess(param, member), typeof(String).GetMethod("Contains", new Type[] { typeof(string) }), constant)
-                        : (Expression)Expression.Equal(Expression.MakeMemberAccess(param, member), constant);
-
-                    if (result == null)
-                        result = expression;
-                    else
-                    {
-                        result = Expression<Func<T, bool>>.Or(result, expression);
-                    }
+                    Expression expression = CreateExpression<T>(value, member);
+                    if (expression == null) continue;
+                    result = result == null ? expression : Expression.Or(result, expression);
                 }
             }
 
             Expression<Func<T, bool>> ret=Expression.Lambda<Func<T, bool>>(result, Expression.Parameter(typeof(T), "i"));
             return ret;
+        }
+
+        private static Expression CreateExpression<T>(object value, PropertyInfo member)
+        {
+            ParameterExpression param = Expression.Parameter(typeof(T), "i");
+            ConstantExpression constant = Expression.Constant(value, value.GetType());
+
+            if(value.GetType()==member.PropertyType)
+            {
+                if(value is string)
+                    return Expression.Call(Expression.MakeMemberAccess(param, member), typeof (String).GetMethod("Contains", new Type[] {typeof (string)}), constant);
+                return Expression.Equal(Expression.MakeMemberAccess(param, member), constant);
+            }
+
+            ConstantExpression constant2 = null;
+            try
+            {
+                constant2 = Expression.Constant(Convert.ChangeType(value, member.PropertyType), member.PropertyType);
+            }
+            catch (InvalidCastException e)
+            {
+                return null;
+            }
+            catch (FormatException e)
+            {
+                return null;
+            }
+
+            return Expression.Equal(Expression.MakeMemberAccess(param, member), constant2);
         }
     }
 }
